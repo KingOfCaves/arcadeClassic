f7bd3147db814774e5aea5d298b5858633c7820e
Nick Wills <37917347+KingOfCaves@users.noreply.github.com>
2018-11-27 12:16:43 -0500

    cleaning up initial comments and adding to readme

--- README.md
+++ README.md
@@ -6,9 +6,28 @@
 * [Credits](#credits)
 
 ## Getting Started
+Clone the main repository and load the index.html file into your browser of choice. Or you can go to Github Pages version [here]().
 
 ## Credits
 
 ### Baseline repository
+> Taken from https://github.com/udacity/frontend-nanodegree-arcade-game
 
-> Takes from https://github.com/udacity/frontend-nanodegree-arcade-game
+### Github Logo
+>Pixel tracing of Github Icon made by https://www.flaticon.com/authors/daniel-bruce from https://flaticon.com
+>
+>Licensed under CC BY 3.0: http://creativecommons.org/licenses/by/3.0/
+
+### Comicoro
+>Note of the author
+>COMICORO by jeti: A pixel font designed after comic book dialogue lettering. The >uppercase is based on traditional all-caps lettering, while the lowercase takes >inspiration from comics that use mixed-case (for instance, in thoughts/narration). 
+>
+>You are free to use this font for personal or commercial projects, all I ask is that >you include credit.
+>
+>Licensed under CC BY 4.0: https://creativecommons.org/licenses/by/4.0/
+>More info: https://fontenddev.com/fonts/comicoro/
+
+## Background
+>https://www.reddit.com/r/wallpapers/comments/5jf8xa/pixel_art_wallpaper_gallery/
+>
+>This is where I downloaded it from but the original artwork is from SNK vs. Capcom CHAOS.
--- js/app.js
+++ js/app.js
@@ -3,10 +3,7 @@ const col = 101;
 
 const reset = document.querySelector(".reset");
 
-// Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.randomSpeed = function(min, max) {
         return col * (Math.round(Math.random() * (max - min) + min));
     }
@@ -21,17 +18,10 @@ var Enemy = function() {
     this.height = row;
     this.width = col;
     this.offset = 11;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.collision = function(collide){
         if (collide){
             player.x = player.startCord.x;
@@ -61,14 +51,10 @@ Enemy.prototype.update = function(dt) {
     this.collision(this.range);
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 var Character = function(){
     this.sprite = 'images/char-boy.png';
     this.startCord = {
@@ -141,9 +127,6 @@ var Character = function(){
         }
     }
 }
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 const player = new Character();
 const enemy1 = new Enemy();
 const enemy2 = new Enemy();
@@ -151,8 +134,6 @@ const enemy3 = new Enemy();
 const allEnemies = [enemy1,enemy2,enemy3];
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
